16008ab34d8091c52479efaf5908b23f0d00d264
Bhushan Ladde <Bhushan Ladde>
2017-11-10 15:55:28 -0800

    video code

--- CSC255_StreamofStream2/app/src/main/java/com/csus/sos/csc255_streamofstream/MainActivity.java
+++ CSC255_StreamofStream2/app/src/main/java/com/csus/sos/csc255_streamofstream/MainActivity.java
@@ -1,7 +1,10 @@
 package com.csus.sos.csc255_streamofstream;
 
+import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.VideoView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +12,14 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        VideoView videoview = (VideoView) findViewById(R.id.myVideo);
+        String vidAddress = "https://archive.org/download/ksnn_compilation_master_the_internet/ksnn_compilation_master_the_internet_512kb.mp4";
+        Uri vidUri = Uri.parse(vidAddress);
+        videoview.setVideoURI(vidUri);
+        videoview.start();
+
+    }
+    public void onButtonClick(View v) {
+
     }
 }
