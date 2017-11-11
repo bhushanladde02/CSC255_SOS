2ca3beb086f0f9bf6bb162cdb1083a1c129b81d5
Bhushan Ladde <Bhushan Ladde>
2017-11-10 16:49:12 -0800

    Code corrected

--- CSC255_StreamofStream2/app/src/main/java/com/csus/sos/csc255_streamofstream/MainActivity.java
+++ CSC255_StreamofStream2/app/src/main/java/com/csus/sos/csc255_streamofstream/MainActivity.java
@@ -1,25 +1,76 @@
 package com.csus.sos.csc255_streamofstream;
 
+import android.media.AudioManager;
+import android.media.MediaPlayer;
 import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.SurfaceHolder;
+import android.view.SurfaceView;
 import android.view.View;
+import android.widget.MediaController;
 import android.widget.VideoView;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements SurfaceHolder.Callback, MediaPlayer.OnPreparedListener {
+
+    private MediaPlayer mediaPlayer;
+    private SurfaceHolder vidHolder;
+    private SurfaceView vidSurface;
+    String vidAddress = "https://archive.org/download/ksnn_compilation_master_the_internet/ksnn_compilation_master_the_internet_512kb.mp4";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        VideoView videoview = (VideoView) findViewById(R.id.myVideo);
-        String vidAddress = "https://archive.org/download/ksnn_compilation_master_the_internet/ksnn_compilation_master_the_internet_512kb.mp4";
-        Uri vidUri = Uri.parse(vidAddress);
-        videoview.setVideoURI(vidUri);
-        videoview.start();
+        MediaController vidControl = new MediaController(this);
 
+        VideoView vidView = (VideoView) findViewById(R.id.myVideo);
+        vidControl.setAnchorView(vidView);
+        vidView.setMediaController(vidControl);
+
+        Uri vidUri = Uri.parse(vidAddress);
+        vidView.setVideoURI(vidUri);
+
+
+        vidSurface = (SurfaceView) findViewById(R.id.surfView);
+        vidHolder = vidSurface.getHolder();
+        vidHolder.addCallback(this);
+        //vidView.start();
+        //mediaPlayer.start();
     }
     public void onButtonClick(View v) {
 
+    }
+    @Override
+    public void surfaceChanged(SurfaceHolder arg0, int arg1, int arg2, int arg3) {
+        // TODO Auto-generated method stub
+    }
+
+    @Override
+    public void surfaceCreated(SurfaceHolder arg0) {
+//setup
+        try {
+            mediaPlayer = new MediaPlayer();
+            mediaPlayer.setDisplay(vidHolder);
+            mediaPlayer.setDataSource(vidAddress);
+            mediaPlayer.prepare();
+            mediaPlayer.setOnPreparedListener(this);
+            mediaPlayer.setAudioStreamType(AudioManager.STREAM_MUSIC);
+        }
+        catch(Exception e){
+            e.printStackTrace();
+        }
+    }
+
+    @Override
+    public void surfaceDestroyed(SurfaceHolder arg0) {
+        // TODO Auto-generated method stub
+    }
+
+    @Override
+    public void onPrepared(MediaPlayer mp) {
+//start playback
+
+       // mediaPlayer.start();
     }
 }
